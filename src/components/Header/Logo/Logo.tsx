17228ad312a2f6280e3a787a5014c76a88d4ce32
Anna <anysofronova@yandex.ru>
2022-07-23 14:57:40 +0300

    feat: Navigation by clicking on the logo 🖱

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,15 +1,18 @@
 import { Unsplash } from "@styled-icons/boxicons-logos";
 
 import styles from "./Logo.module.scss";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div className={styles.logo}>
-      <div>
-        <Unsplash />
+    <Link to={"/home"}>
+      <div className={styles.logo}>
+        <div>
+          <Unsplash />
+        </div>
+        <h1>Unsplash</h1>
       </div>
-      <h1>Unsplash</h1>
-    </div>
+    </Link>
   );
 };
 

9ca761eef5e02c1357c3f59b0f9c1cbde0f03477
Anna <anysofronova@yandex.ru>
2022-07-27 09:33:35 +0300

    feat: Modal Window 🪟

--- src/components/Header/AddAPhoto/AddAPhoto.tsx
+++ src/components/Header/AddAPhoto/AddAPhoto.tsx
@@ -1,10 +1,15 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./AddAPhoto.module.scss";
+import Modal from "../../Modal/Modal";
 
 const AddAPhoto: FC = () => {
+  const [modal, setModal] = useState<boolean>(false);
   return (
     <div className={styles.addAPhoto}>
-      <button>Add a photo</button>
+      <button onClick={() => setModal(true)} className={styles.button}>
+        Add a photo
+      </button>
+      {modal && <Modal setModal={setModal} />}
     </div>
   );
 };

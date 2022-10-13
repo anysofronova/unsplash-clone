dc1214743e45e42c03c3889e65e6cc5ed3840bbe
Anna <anysofronova@yandex.ru>
2022-10-13 15:42:19 +0300

    feat: add image placeholder and change the data request

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,25 +12,28 @@ import Loader from "../../components/UI/Loader/Loader";
 const Home = () => {
   const { isAuth } = useAuth();
   const { id } = useAppSelector((state) => state.authSlice);
-  const [data, setData] = useState<IImage[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [data, setData] = useState<IImage[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  console.log(data);
   useEffect(() => {
     setIsLoading(true);
-    const q = isAuth
-      ? query(collection(db, `users/${id}/photos`))
-      : query(collection(db, "general"));
-    onSnapshot(q, (querySnapshot) => {
-      const photos: IImage[] = [];
-      querySnapshot.forEach((doc) => {
-        photos.push({
-          label: doc.data().label,
-          photoURL: doc.data().photoURL,
-          id: doc.id,
+    const photos: IImage[] = [];
+    const fetchData = async () => {
+      const q = isAuth
+        ? query(collection(db, `users/${id}/photos`))
+        : query(collection(db, "general"));
+      await onSnapshot(q, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          photos.push({
+            label: doc.data().label,
+            photoURL: doc.data().photoURL,
+            id: doc.id,
+          });
         });
         setData(photos);
-        setIsLoading(false);
       });
-    });
+    };
+    fetchData().then(() => setIsLoading(false));
   }, [id, isAuth]);
 
   const breakpointColumns = {
@@ -43,11 +46,9 @@ const Home = () => {
   };
   return (
     <section className={styles.home}>
-      {isLoading ? (
-        <Loader />
-      ) : data && data.length === 0 ? (
-        <div>NO RESULTS</div>
-      ) : (
+      {(isLoading || data === null) && <Loader />}
+      {data?.length === 0 && <div>NO RESULTS</div>}
+      {data && (
         <Masonry
           breakpointCols={breakpointColumns}
           className="myMasonryGrid"
